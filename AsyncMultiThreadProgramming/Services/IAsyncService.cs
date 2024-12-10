20c09803280ce39eeda2960778e4025a713225dc
neominal.solutions <neominal.solutions@gmail.com>
2024-12-10 13:47:20 +0300

    WhenAll ile multiple Tasks yönetimi

--- AsyncMultiThreadProgramming/Services/IAsyncService.cs
+++ AsyncMultiThreadProgramming/Services/IAsyncService.cs
@@ -22,10 +22,18 @@
       // Not: Asenkron kod blokları içerisinde gerçekleşen hata durumlarını yakalamak için task.Fromexception döndürmek zorundayız. Senkron kod gibi asenkron kod hatalarında catch otomatik bloğu tetiklenemez.
       var tasks1 = Task.Run(() =>
       {
-        int a = 10;
+        int a = 0;
         int b = 10;
 
         int z = b / a; // 0 bölünme logic hatası
+      });
+
+      var tasks2 = Task.Run(() =>
+      {
+        int[] numbers = new int[1];
+        numbers[0] = 1;
+        numbers[1] = 2;
+
       });
 
     
@@ -36,11 +44,16 @@
         return await Task.FromCanceled<ProductDto>(cancellationToken);
       }
 
-      await tasks1; // running state kurtamamız lazım, tasks1.IsFaulted state geçmesi için işlemin bitmesini bekle.
 
-      if (tasks1.IsFaulted) // Exceptional Caselerin takibini ise IsFaulted ile yapacağız.
+      // await tasks1; // running state kurtamamız lazım, tasks1.IsFaulted state geçmesi için işlemin bitmesini bekle.
+       // when all non-blocking olarak çalışır. Tüm taskların aynı anda çözülmesini sağlar.
+
+      var taskResult = Task.WhenAll(tasks1, tasks2); // multiple task single failure point
+      await taskResult;
+
+      if (taskResult.IsFaulted) // Exceptional Caselerin takibini ise IsFaulted ile yapacağız.
       {
-        return await Task.FromException<ProductDto>(tasks1.Exception); // bu sayede controllerdaki catch'i tetikleyebiliriz. Kendi custom servisimizde exception durumlarının yönetimini yapmak zorundayız.
+        return await Task.FromException<ProductDto>(taskResult.Exception); // bu sayede controllerdaki catch'i tetikleyebiliriz. Kendi custom servisimizde exception durumlarının yönetimini yapmak zorundayız.
       }
       else
       {
